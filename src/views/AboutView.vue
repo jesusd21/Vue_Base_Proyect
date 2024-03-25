daf77591962e9c162de9fcb6b8ff1ad0422414ea
jesus.ramirez <jesusd211@gmail.com>
2024-03-25 15:51:32 +0100

    paleta

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,12 +2,17 @@
 import { ref, onMounted } from 'vue';
 
 const paleta = ref([]);
+const paletasReservadas = ref([])
 const colorInput = ref('');
+const showTooltip = ref(false)
+const tooltipPosition = ref({ x: 0, y: 0 })
 
-onMounted(()=>{
+onMounted(() => {
   generarPaletaMonocromaticaAleatoria()
 })
-
+function updateTooltipPosition(event) {
+  this.tooltipPosition = { x: event.clientX + 40, y: event.clientY - 20 }; // Ajuste la posición y - 20
+}
 const generarColorAleatorio = () => {
   const letras = '0123456789ABCDEF';
   let color = '#';
@@ -122,41 +127,113 @@ const validarColorHexadecimal = (color) => {
 };
 
 function copyColor(color) {
-      navigator.clipboard.writeText(color);
-      Vue.prototype.$toast.success('Color copiado al portapapeles');
-    }
+  navigator.clipboard.writeText(color);
+  Vue.prototype.$toast.success('Color copiado al portapapeles');
+}
+function reservar() {
+  if( paletasReservadas.value.length===2){
+    return
+  }
+  paletasReservadas.value.push(paleta.value)
+}
 </script>
 
 <template>
-  <div class="h-[70rem]">
+  <div class="h-[100%] mb-16 p-60">
     <div class="flex justify-center mt-24 text-[#386379] opacity-55 p-[90px] text-[4rem]">
       <p>
-        Generador de Paletas de Colores 
+        Generador de Paletas de Colores
       </p>
     </div>
     <div class=" flex justify-center mt-14">
-      <input v-model="colorInput" type="text" placeholder="#FFFFFF" class="p-2 mr-2 border rounded">
-      <button @click="generarPaletaPersonalizada" class="bg-[#386379] opacity-55 hover:bg-[#386379] hover:opacity-100 text-white font-bold py-2 px-4 rounded">Generar Paleta Personalizada</button>
+      <input v-model="colorInput" type="text" placeholder="#FFFFFF" class="p-2 mr-2 border rounded border-[#386379]">
+      <button @click="generarPaletaPersonalizada"
+        class="bg-[#386379] opacity-55 hover:bg-[#386379] hover:opacity-100 text-white font-bold py-2 px-4 rounded">Generar
+        Paleta Personalizada</button>
     </div>
     <div class="mt-4 flex justify-center">
-      <button @click="generarPaletaAleatoria" class="bg-[#386379] opacity-55 hover:bg-[#386379] hover:opacity-100 text-white font-bold py-2 px-4 rounded">Generar Paleta Aleatoria</button>
-      <button @click="generarPaletaMonocromaticaAleatoria" class="bg-[#386379] opacity-55 hover:bg-[#386379] hover:opacity-100 text-white font-bold py-2 px-4 rounded ml-4">Generar Paleta Monocromática Aleatoria</button>
+      <button @click="generarPaletaAleatoria"
+        class="bg-[#386379] opacity-55 hover:bg-[#386379] hover:opacity-100 text-white font-bold py-2 px-4 rounded">Generar
+        Paleta Aleatoria</button>
+      <button @click="generarPaletaMonocromaticaAleatoria"
+        class="bg-[#386379] opacity-55 hover:bg-[#386379] hover:opacity-100 text-white font-bold py-2 px-4 rounded ml-4">Generar
+        Paleta Monocromática Aleatoria</button>
     </div>
-    <div class="flex justify-center mt-8">
-      <div  v-for="(color, index) in paleta" :key="index" class="w-32 h-96 text-white hover:text-black cursor-pointer" 
-      style="display: flex; justify-content: center; align-items: center;" 
-      :style="{ backgroundColor: color }" @click="copyColor(color)">{{color}}</div>
+    <div class="mt-4 flex justify-center">
+      <button @click="reservar"
+        class="bg-[#386379] opacity-55 hover:bg-[#386379] hover:opacity-100 text-white font-bold py-2 px-4 rounded ml-4">Reservar</button>
     </div>
+    <div class="flex justify-center mt-8 ">
+      <div @mouseenter="showTooltip = true" @mouseleave="showTooltip = false" @mousemove="updateTooltipPosition($event)"
+        v-for="(color, index) in paleta" :key="index"
+        class="color-box w-32 h-96 text-white hover:text-black cursor-pointer"
+        style="display: flex; justify-content: center; align-items: center;" :style="{ backgroundColor: color }"
+        @click="copyColor(color)">{{ color }}
+        <div class="tooltip" v-if="showTooltip"
+          :style="{ top: tooltipPosition.y + 'px', left: tooltipPosition.x + 'px' }">Copy</div>
+      </div>
+    </div>
+    <h2 class="text-[#386379] opacity-55 text-2xl font-bold mb-4 mt-24 flex justify-center" v-if="paletasReservadas.length > 0">Paletas Reservadas</h2>
+    <div class="flex justify-center mt-8" v-if="paletasReservadas.length > 0">
+ 
+  <template v-for="(palet, paletIndex) in paletasReservadas" :key="paletIndex">
+    <div class="flex w-1/2 mb-8 ml-10">
+      <template v-for="(color, colorIndex) in palet" :key="colorIndex">
+        <div @mouseenter="showTooltip = true" @mouseleave="showTooltip = false"
+          @mousemove="updateTooltipPosition($event)" 
+          class="color-box w-32 h-96  text-white hover:text-black cursor-pointer flex-nowrap"
+          style="display: flex; justify-content: center; align-items: center;" 
+          :style="{ backgroundColor: color }"
+          @click="copyColor(color)">
+          {{ color }}
+          <div class="tooltip" v-if="showTooltip"
+            :style="{ top: tooltipPosition.y + 'px', left: tooltipPosition.x + 'px' }">Copy</div>
+        </div>
+      </template>
+    </div>
+
+  </template>
+</div>
+
+
+
+
   </div>
 </template>
 
 
 <style>
 .parentContainer {
-    height: 100vh; /* Adjust to your needs */
-    background-color: #c4c6bf;
+  height: 100vh;
+  /* Adjust to your needs */
+  background-color: #c4c6bf;
 }
+
 .bg-color {
   background-color: #f3f4f6;
+}
+
+.color-box {
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.tooltip {
+  position: absolute;
+  top: 100%;
+  left: 50%;
+  transform: translateX(-50%);
+  background-color: rgba(0, 0, 0, 0.8);
+  color: white;
+  padding: 4px 8px;
+  border-radius: 4px;
+  font-size: 12px;
+  display: none;
+}
+
+.color-box:hover .tooltip {
+  display: block;
 }
 </style>
